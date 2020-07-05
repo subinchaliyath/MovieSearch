7678b9fdd120be9dbde6ef7327feca27528e221d
Subin Chaliyath <subinchaliyath@gmail.com>
2020-07-05 20:38:45 +0530

    Updates

---
+++ precache-manifest.dba80025bb4fb5dcc55247ef887aa414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709b402828c53afc31b2b14094a692d9",
+    "url": "/MovieSearch/index.html"
+  },
+  {
+    "revision": "49be568a0a5c1f88d854",
+    "url": "/MovieSearch/static/css/main.d073a799.chunk.css"
+  },
+  {
+    "revision": "2af47401fa96e98f2163",
+    "url": "/MovieSearch/static/js/2.06751b03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MovieSearch/static/js/2.06751b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49be568a0a5c1f88d854",
+    "url": "/MovieSearch/static/js/main.1835ad8e.chunk.js"
+  },
+  {
+    "revision": "441a7c66a3cf700ca42a",
+    "url": "/MovieSearch/static/js/runtime-main.1f5869bd.js"
+  }
+]);
